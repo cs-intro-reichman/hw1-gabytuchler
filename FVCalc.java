185cf425a397b8535e93feb32bc7b2d8c428e0ce
gabytuchler <gaby.tuchler@gmail.com>
2025-11-05 19:54:06 +0200

    Add files via upload

--- FVCalc.java
+++ FVCalc.java
@@ -2,15 +2,19 @@
 public class FVCalc {
 	public static void main(String[] args){
 
+		//הקלט
 		int currentvalue = Integer.parseInt(args[0]);
 		double rate = Double.parseDouble(args[1]);
         double ratepercent = rate / 100.0;
 		int years = Integer.parseInt(args[2]);
 
+		//חישוב סכום עתידי 
 		double futurevalueDouble = (currentvalue * (Math.pow((1.0 + ratepercent), years)));
 		
+		// העברה של סכום עתידי לשלם
 		int futurevalueInt = (int) futurevalueDouble;
 		
+		//הדפסת פלט
 		System.out.println("After " + years + " years, $" + currentvalue + 
                            " saved at " + ratepercent + "% will yield $" + 
                            futurevalueInt);
